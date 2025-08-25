12212810ad65e2053ad0f062d0851b7573aa1045
deadshoot333 <arqam343almas@gmail.com>
2025-08-26 01:17:23 +0600

    GET,POST added in community/post

---
+++ server/models/post.model.js
@@ -0,0 +1,44 @@
+const { pool } = require("../config/connection");
+
+async function getAllPosts() {
+  const result = await pool.query(
+    "SELECT p.id, p.content, p.post_type, p.tags, p.images, p.likes, p.comments_count, p.is_liked, p.created_at, u.id AS user_id, u.user_name, u.user_image, u.user_location FROM public.posts p JOIN public.users u ON p.user_id = u.id ORDER BY p.created_at DESC"
+  );
+  return result.rows;
+}
+async function likePost(postId) {
+  const result = await pool.query(
+    "UPDATE public.posts SET likes=likes+1, is_liked=true WHERE id=$1 RETURNING *",
+    [postId]
+  );
+  return result.rows[0];
+}
+async function unlikePost(postId) {
+  const result = await pool.query(
+    `
+    UPDATE public.posts 
+    SET likes = GREATEST(likes - 1, 0), is_liked = false 
+    WHERE id = $1 
+    RETURNING *`,
+    [postId]
+  );
+  return result.rows[0];
+}
+async function addPost(data) {
+  const { userId, content, postType, tags, images } = data;
+
+  const result = await pool.query(
+    `INSERT INTO public.posts (user_id, content, post_type, tags, images) 
+     VALUES ($1, $2, $3, $4, $5)
+     RETURNING *`,
+    [userId, content, postType, tags, images]
+  );
+
+  return result.rows[0];
+}
+module.exports = {
+  getAllPosts,
+  likePost,
+  unlikePost,
+  addPost
+};
--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -1,24 +1,25 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const { getAllPosts, addPost } = require("../models/post.model");
+const router = express.Router();
 
-router.get("/get-posts",async(req,res)=>{
-    const {user_id} = req.body;
-    try {
-        const posts = await getPosts(user_id)
-        res.json({posts})    
-    } catch (error) {
-        console.error("Error gettting posts:", error);
-        res.status(500).json({ error: "Failed to get posts" });
-    }
-})
-route.post("/post",async(req,res)=>{
-    const {user_id} = req.body;
-    try {
-        const post = await addPost(user_id)
-        res.json({post})
-    } catch (error) {
-        console.error("Error gettting posts:", error);
-        res.status(500).json({ error: "Failed to get posts" });
-    }
-})
-module.exports = router
+router.get("/get-posts", async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const posts = await getAllPosts(user_id);
+    res.json({ posts });
+  } catch (error) {
+    console.error("Error gettting posts:", error);
+    res.status(500).json({ error: "Failed to get posts" });
+  }
+});
+router.post("/post", async (req, res) => {
+  try {
+    const post = await addPost(req.body);
+    res.json({ post });
+  } catch (error) {
+    console.error("Error gettting posts:", error);
+    res.status(500).json({ error: "Failed to get posts" });
+  }
+});
+
+module.exports = router;
