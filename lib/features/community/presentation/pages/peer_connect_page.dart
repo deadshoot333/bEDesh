c8030c00b9a9d3e766262ea5609755d5b3f54ecb
anika <anika.tasnim0302@gmail.com>
2025-09-17 02:28:38 +0600

    backbutton of peer connect working

--- lib/features/community/presentation/pages/peer_connect_page.dart
+++ lib/features/community/presentation/pages/peer_connect_page.dart
@@ -12,6 +12,7 @@ import '../../../profile/presentation/pages/profile_page.dart';
 import '../../../../core/models/user.dart';
 import '../../../../core/services/storage_service.dart';
 import '../../../../core/constants/api_constants.dart';
+import '../../../../MainNavigationPage.dart';
 
 class PeerConnectPage extends StatefulWidget {
   const PeerConnectPage({super.key});
@@ -178,6 +179,21 @@ class _PeerConnectPageState extends State<PeerConnectPage>
     }
   }
 
+  void _handleBackNavigation() {
+    // Check if we can pop (go back to previous page)
+    if (Navigator.of(context).canPop()) {
+      Navigator.of(context).pop();
+    } else {
+      // If no previous page (accessed from main nav tabs), go to home tab
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (context) => const MainNavigationPage(),
+        ),
+        (route) => false,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -251,7 +267,7 @@ class _PeerConnectPageState extends State<PeerConnectPage>
                     size: 20,
                   ),
                   onPressed: () {
-                    Navigator.pop(context);
+                    _handleBackNavigation();
                   },
                 ),
               ),
